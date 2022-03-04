4dad033fcd7c428c8f3f282bb7213a227ddc5e07
OpenByte <development.openbyte@gmail.com>
2022-03-04 19:32:16 +0100

    Fix foreground example

--- examples/foreground.rs
+++ examples/foreground.rs
@@ -1,6 +1,6 @@
 use std::{
     io,
-    ptr::{self, NonNull},
+    ptr::{self, NonNull}, num::NonZeroUsize,
 };
 
 use winapi::{
@@ -30,34 +30,36 @@ async fn main() {
                     .map_or_else(ptr::null_mut, NonNull::as_ptr),
             )
             .unwrap();
-            println!("{}", title);
+            println!("{:?}", title);
         }
     }
 
     hook.unhook().await.unwrap();
 }
 
-fn get_window_text_length(window: HWND) -> io::Result<usize> {
+fn get_window_text_length(window: HWND) -> io::Result<Option<NonZeroUsize>> {
     unsafe { SetLastError(0) };
     let result = unsafe { GetWindowTextLengthW(window) };
     if result == 0 && unsafe { GetLastError() } != 0 {
         Err(io::Error::last_os_error())
     } else {
-        Ok(result as _)
+        Ok(NonZeroUsize::new(result as usize))
     }
 }
 
-fn get_window_text(window: HWND) -> io::Result<String> {
-    let length = get_window_text_length(window)?;
-    if length == 0 {
-        return Ok(String::new());
-    }
-    
-    let mut text = vec![0u16; length + 1];
-    let result = unsafe { GetWindowTextW(window, text.as_mut_ptr(), text.len() as i32) };
+fn get_window_text(window: HWND) -> io::Result<Option<String>> {
+    let text_len = if let Some(length) = get_window_text_length(window)? {
+        length.get()
+    } else {
+        return Ok(None);
+    };
+
+    let mut text = Vec::with_capacity(text_len + 1); // +1 for null terminator
+    let result = unsafe { GetWindowTextW(window, text.as_mut_ptr(), text.capacity() as i32) };
     if result != 0 {
-        let text = String::from_utf16_lossy(&text[..text.len()-1]);
-        Ok(text)
+        unsafe { text.set_len(text_len) };
+        let text = String::from_utf16_lossy(&text);
+        Ok(Some(text))
     } else {
         Err(io::Error::last_os_error())
     }
